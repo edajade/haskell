38f16e047a54cb0b31a72cd0d8d506d33d0be17b
Jade O'Neill <jadicorn@gmail.com>
2014-01-28 17:15:20 +1100

    Brainfuck can handle IO and has a parser

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,9 +1,7 @@
 -- Brainfuck interpreter in Haskell
--- The eval function can handle all of the instructions except IO.
--- The easiest solution would be to handle sequences inside the IO monad too
--- Also you need a simple parser
 
 import Data.Char (ord, chr)
+import System.IO (hSetEcho, stdin)
 
 -- A brainfuck program would be represented as an Expr (usually a Sequence),
 -- and a Loop is allowed to contain a sequence too
@@ -33,55 +31,106 @@ update f (Zipper before (x:after)) = Zipper before (new:after)
 initialMemory = fromList $ repeat 0
 
 eval :: Expr -> Memory -> Memory
-eval l@(Loop s) m = 
-    if current m == 0 then
-        m
-    else
-        let
-            m2 = eval s m
-            m3 = eval l m2
-        in
-            m3
 
-eval (Sequence is) m = evalSeq is m
 eval Plus m = update (\x -> x+1) m
 eval Minus m = update (\x -> x-1) m
 eval Back m = back m
 eval Forward m = forward m
 
-evalSeq :: [Expr] -> Memory -> Memory
+-- helper function for evaluating sequences
+evalSeq :: [Expr] -> Memory -> IO Memory
 evalSeq (i:is) m =
-    let
-        m' = eval i m
-    in
-        evalSeq is m'
-evalSeq [] m = m
+    do 
+       m' <- evalIO i m
+       evalSeq is m'
+evalSeq [] m = do
+    return m
 
-
+-- main eval function which can handle any instruction and delegates some to evalSe and eval
 evalIO :: Expr -> Memory -> IO Memory
-evalIO e m
-    | e == Input =
-        do
-            char <- getChar
-            let value = ord char
-            let m' = update (\x -> value) m
-            return m'
-    | e == Output =
-        do
-            let value = current m
-            let char = chr value
-            putChar $ char
-            return m
-    | otherwise = undefined
+evalIO (Input) m =
+    do
+        char <- getChar
+        let value = ord char
+        let m' = update (\x -> value) m
+        return m'
+evalIO (Output) m =
+    do
+        let value = current m
+        let char = chr value
+        putChar (if ord char /= 0 then char else '0')
+        return m
+evalIO (Sequence is) m = evalSeq is m
+evalIO l@(Loop s) m = evalLoop s m
+evalIO i m = do
+    let m' = eval i m
+    return m'
 
--- +[-]
-sampleProgram :: Expr
-sampleProgram = Sequence [
+evalLoop :: Expr -> Memory -> IO Memory
+evalLoop s m =
+    if current m == 0 then
+        return m
+    else
+        do
+            m2 <- evalIO s m
+            evalLoop s m2
+
+-- Convert Brainfuck programs from text to abstract syntax tree and back again
+-- Assume that every loop contains a Sequence even if it's one instruction. And the
+-- root node of the tree is always Sequence.
+-- Sequence [Expr] | Plus | Minus | Back | Forward | Loop Sequence | Input | Output
+brainfuckCharacters = "+-<>,.[]"
+
+parse :: String -> Expr
+parse s = parseSequence $ filter (\c -> elem c brainfuckCharacters) s
+
+parseSequence s = Sequence $ parse' s where
+    parse' "" = []
+    parse' s = 
+        let (e, rest) = parseExpr s in
+            (e : parse' rest)
+
+parseExpr :: String -> (Expr, String)
+parseExpr ('[':cs) = (Loop $ parseSequence contents, rest)  where
+    -- split the contents of the loop from the rest of the program (excluding the closing bracket)
+    contents = takeWhile (/=']') cs
+    rest = tail $ dropWhile (/=']') cs
+parseExpr (c:cs) = (parseChar c, cs)
+
+parseChar c =
+   case c of
+    '+' -> Plus
+    '-' -> Minus
+    '<' -> Back
+    '>' -> Forward
+    ',' -> Input
+    '.' -> Output
+
+
+-- +[.-]
+sampleProgram :: String
+sampleProgram = "+[.-]"
+
+sampleProgramAST :: Expr
+sampleProgramAST = Sequence [
     Plus,
-    Loop Minus]
+    Loop (Sequence [
+        Output,
+        Minus
+        ])
+    ]
+
+-- input/echo a specific number of characters
+-- ++++++++++++++++++++++++[>,.<-]
+
+evalString :: String -> IO ()
+evalString s = do
+    finalMemory <- evalIO (parse s) initialMemory
+    return ()
 
 main :: IO ()
 main = do
-    let finalMemory = eval sampleProgram initialMemory
+    hSetEcho stdin False
+    finalMemory <- evalIO sampleProgramAST initialMemory
     return ()
 
